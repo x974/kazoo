00d60509ea193a916c333c0f9ccd7926a6b6b42a
James Aimonetti <james@2600hz.org>
2012-05-01 19:43:50 -0700

    riak: rework wh_cache to use riak as its backend instead of a dict (well, uses a dict for keys at the moment)

--- lib/whistle-1.0.0/src/wh_cache.erl
+++ lib/whistle-1.0.0/src/wh_cache.erl
@@ -1,10 +1,11 @@
 %%%-------------------------------------------------------------------
-%%% @author James Aimonetti <james@2600hz.org>
-%%% @copyright (C) 2011, VoIP INC
+%%% @copyright (C) 2011-2012, VoIP INC
 %%% @doc
 %%% Simple cache server
 %%% @end
-%%% Created : 30 Mar 2011 by James Aimonetti <james@2600hz.org>
+%%% @contributors
+%%%   James Aimonetti
+%%%   Karl Anderson
 %%%-------------------------------------------------------------------
 -module(wh_cache).
 
@@ -38,6 +39,12 @@
 -define(EXPIRES, 3600). %% an hour
 -define(EXPIRE_CHECK, 30000).
 
+-record(state, {
+          pb_pid :: pid() %% PB connection to Riak
+         ,keys :: dict() %% list of keys and expiration
+         ,bucket :: ne_binary() %% name of the cache proc
+         }).
+
 %%%===================================================================
 %%% API
 %%%===================================================================
@@ -69,73 +76,82 @@ store(K, V) ->
 store(K, V, Fun) when is_function(Fun, 3) ->
     store(K, V, ?EXPIRES, Fun);
 store(K, V, T) ->
-    gen_server:cast(?SERVER, {store, K, V, T, undefined}).
+    store_local(?SERVER, K, V, T).
 
 store(K, V, T, Fun) when is_function(Fun, 3) ->
-    gen_server:cast(?SERVER, {store, K, V, T, Fun}).
+    store_local(?SERVER, K, V, T, Fun).
 
 -spec peek/1 :: (term()) -> {'ok', term()} | {'error', 'not_found'}.
 peek(K) ->
-    gen_server:call(?SERVER, {peek, K}).
+    peek_local(?SERVER, K).
 
 -spec fetch/1 :: (term()) -> {'ok', term()} | {'error', 'not_found'}.
 fetch(K) ->
-    gen_server:call(?SERVER, {fetch, K}).
+    fetch_local(?SERVER, K).
 
 -spec erase/1 :: (term()) -> 'ok'.
 erase(K) ->
-    gen_server:cast(?SERVER, {erase, K}).
+    erase_local(?SERVER, K).
 
 -spec flush/0 :: () -> 'ok'.
 flush() ->
-    gen_server:cast(?SERVER, {flush}).
+    flush_local(?SERVER).
 
 -spec fetch_keys/0 :: () -> [term(),...] | [].
 fetch_keys() ->
-    gen_server:call(?SERVER, fetch_keys).
+    fetch_keys_local(?SERVER).
 
 -spec filter/1 :: (fun((term(), term()) -> boolean())) -> proplist().
 filter(Pred) when is_function(Pred, 2) ->
-    gen_server:call(?SERVER, {filter, Pred}).
+    filter_local(?SERVER, Pred).
 
 %% Local cache API
--spec store_local/3 :: (pid(), term(), term()) -> 'ok'.
--spec store_local/4 :: (pid(), term(), term(), pos_integer() | 'infinity' | function() | {atom(), atom()}) -> 'ok'.
--spec store_local/5 :: (pid(), term(), term(), pos_integer() | 'infinity', function() | {atom(), atom()}) -> 'ok'.
+-spec store_local/3 :: (pid() | ?SERVER, term(), term()) -> 'ok'.
+-spec store_local/4 :: (pid() | ?SERVER, term(), term(), pos_integer() | 'infinity' | function() | {atom(), atom()}) -> 'ok'.
+-spec store_local/5 :: (pid() | ?SERVER, term(), term(), pos_integer() | 'infinity', function() | {atom(), atom()}) -> 'ok'.
 
-store_local(Srv, K, V) when is_pid(Srv) ->
+store_local(Srv, K, V) when is_pid(Srv) orelse Srv =:= ?SERVER ->
     store_local(Srv, K, V, ?EXPIRES).
 
-store_local(Srv, K, V, Fun) when is_pid(Srv), is_function(Fun, 3) ->
+store_local(Srv, K, V, Fun) when is_pid(Srv) orelse Srv =:= ?SERVER, is_function(Fun, 3) ->
     store_local(Srv, K, V, ?EXPIRES, Fun);
-store_local(Srv, K, V, T) when is_pid(Srv) ->
-    gen_server:cast(Srv, {store, K, V, T, undefined}).
+store_local(Srv, K, V, T) when is_pid(Srv) orelse Srv =:= ?SERVER ->
+    gen_server:cast(Srv, {store, encode(K), encode(V), T, undefined}).
 
-store_local(Srv, K, V, T, Fun) when is_pid(Srv), is_function(Fun, 3) ->
-    gen_server:cast(Srv, {store, K, V, T, Fun}).
+store_local(Srv, K, V, T, Fun) when is_pid(Srv) orelse Srv =:= ?SERVER, is_function(Fun, 3) ->
+    gen_server:cast(Srv, {store, encode(K), encode(V), T, Fun}).
 
--spec peek_local/2 :: (pid(), term()) -> {'ok', term()} | {'error', 'not_found'}.
-peek_local(Srv, K) when is_pid(Srv) ->
-    gen_server:call(Srv, {peek, K}).
+-spec peek_local/2 :: (pid() | ?SERVER, term()) -> {'ok', term()} | {'error', 'not_found'}.
+peek_local(Srv, K) when is_pid(Srv) orelse Srv =:= ?SERVER ->
+    case gen_server:call(Srv, {peek, encode(K)}) of
+        {ok, V} -> {ok, decode(V)};
+        E -> E
+    end.
 
--spec fetch_local/2 :: (pid(), term()) -> {'ok', term()} | {'error', 'not_found'}.
-fetch_local(Srv, K) when is_pid(Srv) ->
-    gen_server:call(Srv, {fetch, K}).
+-spec fetch_local/2 :: (pid() | ?SERVER, term()) -> {'ok', term()} | {'error', 'not_found'}.
+fetch_local(Srv, K) when is_pid(Srv) orelse Srv =:= ?SERVER ->
+    case gen_server:call(Srv, {fetch, encode(K)}) of
+        {ok, V} -> {ok, decode(V)};
+        E -> E
+    end.
 
--spec erase_local/2 :: (pid(), term()) -> 'ok'.
-erase_local(Srv, K) when is_pid(Srv) ->
-    gen_server:cast(Srv, {erase, K}).
+-spec erase_local/2 :: (pid() | ?SERVER, term()) -> 'ok'.
+erase_local(Srv, K) when is_pid(Srv) orelse Srv =:= ?SERVER ->
+    gen_server:cast(Srv, {erase, encode(K)}).
 
--spec flush_local/1 :: (pid()) -> 'ok'.
-flush_local(Srv) when is_pid(Srv) ->
+-spec flush_local/1 :: (pid() | ?SERVER) -> 'ok'.
+flush_local(Srv) when is_pid(Srv) orelse Srv =:= ?SERVER ->
     gen_server:cast(Srv, {flush}).
 
--spec fetch_keys_local/1 :: (pid()) -> [term(),...] | [].
-fetch_keys_local(Srv) when is_pid(Srv) ->
-    gen_server:call(Srv, fetch_keys).
+-spec fetch_keys_local/1 :: (pid() | ?SERVER) -> [term(),...] | [].
+fetch_keys_local(Srv) when is_pid(Srv) orelse Srv =:= ?SERVER ->
+    case gen_server:call(Srv, fetch_keys) of
+        L when is_list(L) -> [decode(K) || K <- L];
+        E -> E
+    end.
 
--spec filter_local/2 :: (pid(), fun((term(), term()) -> boolean())) -> proplist().
-filter_local(Srv, Pred)  when is_pid(Srv) andalso is_function(Pred, 2) ->
+-spec filter_local/2 :: (pid() | ?SERVER, fun((term(), term()) -> boolean())) -> proplist().
+filter_local(Srv, Pred) when (is_pid(Srv) orelse Srv =:= ?SERVER) andalso is_function(Pred, 2) ->
     gen_server:call(Srv, {filter, Pred}).
 
 %%%===================================================================
@@ -157,7 +173,14 @@ init([Name]) ->
     put(callid, ?LOG_SYSTEM_ID),
     _ = erlang:send_after(?EXPIRE_CHECK, self(), flush),
     lager:debug("started new cache proc: ~s", [Name]),
-    {ok, dict:new()}.
+
+    RiakNode = "db001-qa-vb.2600hz.com",
+    RiakPort = 8087,
+    lager:debug("connecting to ~s:~b", [RiakNode, RiakPort]),
+
+    {ok, Pid} = riakc_pb_socket:start_link(RiakNode, RiakPort),
+
+    {ok, #state{bucket=wh_util:to_binary(Name), keys=dict:new(), pb_pid=Pid}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -173,22 +196,43 @@ init([Name]) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_call({peek, K}, _, Dict) ->
+handle_call({peek, K}, _, #state{keys=Dict, pb_pid=Pid, bucket=Bucket}=State) ->
     case dict:find(K, Dict) of
-        {ok, {_, V, _, _}} -> {reply, {ok, V}, Dict};
-        error -> {reply, {error, not_found}, Dict}
+        {ok, {_, _, _, _}} ->
+            case riakc_pb_socket:get(Pid, Bucket, K) of
+                {ok, O} -> {reply, {ok, riakc_obj:get_value(O)}, State, hibernate};
+                _ -> {reply, {error, not_found}, State}
+            end;
+        error -> {reply, {error, not_found}, State}
     end;
-handle_call({fetch, K}, _, Dict) ->
+handle_call({fetch, K}, _, #state{keys=Dict, pb_pid=Pid, bucket=Bucket}=State) ->
     case dict:find(K, Dict) of
-        {ok, {infinity=T, V, T, _}} -> {reply, {ok, V}, Dict};
-        {ok, {_, V, T, F}} -> {reply, {ok, V}, dict:update(K, fun(_) -> {wh_util:current_tstamp()+T, V, T, F} end, Dict), hibernate};
-        error -> {reply, {error, not_found}, Dict}
+        {ok, {infinity=T, _, T, _}} ->
+            case riakc_pb_socket:get(Pid, Bucket, K) of
+                {ok, O} -> {reply, {ok, riakc_obj:get_value(O)}, State, hibernate};
+                _ -> {reply, {error, not_found}, State}
+            end;
+        {ok, {_, _, T, F}} ->
+            case riakc_pb_socket:get(Pid, Bucket, K) of
+                {ok, O} -> {reply
+                            ,{ok, riakc_obj:get_value(O)}
+                            ,State#state{keys=dict:update(K, fun(_) ->
+                                                                     {wh_util:current_tstamp()+T, ok, T, F} 
+                                                             end, Dict)
+                                        }
+                            ,hibernate
+                           };
+                _ -> {reply, {error, not_found}, State}
+            end;
+        error -> {reply, {error, not_found}, State}
     end;
-handle_call(fetch_keys, _, Dict) ->
-    {reply, dict:fetch_keys(Dict), Dict};
-handle_call({filter, Pred}, _, Dict) ->
-    KV = dict:map(fun(_, {_,V,_, _}) -> V end, Dict),
-    {reply, dict:to_list(dict:filter(Pred, KV)), Dict}.
+handle_call(fetch_keys, _, #state{keys=Dict}=State) ->
+    {reply, dict:fetch_keys(Dict), State};
+
+handle_call({filter, _Pred}, _, State) ->
+    {reply, {error, not_implemented}, State}.
+    %% KV = dict:map(fun(_, {_,V,_, _}) -> V end, Dict),
+    %% {reply, dict:to_list(dict:filter(Pred, KV)), Dict}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -200,24 +244,42 @@ handle_call({filter, Pred}, _, Dict) ->
 %%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_cast({store, K, V, infinity=T, F}, Dict) ->
-    {noreply, dict:store(K, {T, V, T, F}, Dict), hibernate};
-handle_cast({store, K, V, T, F}, Dict) ->
-    {noreply, dict:store(K, {wh_util:current_tstamp()+T, V, T, F}, Dict), hibernate};
-handle_cast({erase, K}, Dict) ->
+handle_cast({store, K, V, infinity=T, F}, #state{keys=Dict, pb_pid=Pid, bucket=Bucket}=State) ->
+    Oa = riakc_obj:new(Bucket, K, V),
+    riakc_pb_socket:put(Pid, Oa, []),
+    {noreply, State#state{keys=dict:store(K, {T, ok, T, F}, Dict)}, hibernate};
+
+handle_cast({store, K, V, T, F}, #state{keys=Dict, pb_pid=Pid, bucket=Bucket}=State) ->
+    Oa = riakc_obj:new(Bucket, K, V),
+    riakc_pb_socket:put(Pid, Oa, []),
+    {noreply, State#state{keys=dict:store(K, {wh_util:current_tstamp()+T, ok, T, F}, Dict)}, hibernate};
+
+handle_cast({erase, K}, #state{keys=Dict, pb_pid=Pid, bucket=Bucket}=State) ->
     ok = case dict:find(K, Dict) of
              {ok, {_, _, _, undefined}} -> ok;
-             {ok, {_, V, _, F}} -> spawn(fun() -> F(K, V, erase) end), ok;
+             {ok, {_, _, _, F}} ->
+                 spawn(fun() ->
+                               {ok, Oa} = riakc_pb_socket:get(Pid, Bucket, K),
+                               _ = riakc_pb_socket:delete(Pid, Bucket, K),
+                               F(decode(K), decode(riakc_obj:get_value(Oa)), erase)
+                       end),
+                 ok;
              _ -> ok
          end,
-    {noreply, dict:erase(K, Dict), hibernate};
-handle_cast({flush}, Dict) ->
+    {noreply, State#state{keys=dict:erase(K, Dict)}, hibernate};
+
+handle_cast({flush}, #state{keys=Dict, pb_pid=Pid, bucket=Bucket}=State) ->
     _ = [(fun({_, {_, _, _, undefined}}) -> ok;
-             ({K, {_, V, _, F}}) -> spawn(fun() -> F(K, V, flush) end) 
+             ({K, {_, _, _, F}}) ->
+                  spawn(fun() ->
+                                {ok, Oa} = riakc_pb_socket:get(Pid, Bucket, K),
+                                _ = riakc_pb_socket:delete(Pid, Bucket, K),
+                                F(decode(K), decode(riakc_obj:get_value(Oa)), flush)
+                        end)
           end)(Elem)
          || Elem <- dict:to_list(Dict) 
         ],
-    {noreply, dict:new(), hibernate}.
+    {noreply, State#state{keys=dict:new()}, hibernate}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -229,17 +291,21 @@ handle_cast({flush}, Dict) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_info(flush, Dict) ->
+handle_info(flush, #state{keys=Dict, pb_pid=Pid, bucket=Bucket}=State) ->
     Now = wh_util:current_tstamp(),
     Filter = fun(_, {infinity, _, _, _}) -> true;
                 (_, {T, _, _, _}) when Now < T -> true;
                 (_, {_, _, _, undefined}) -> false;
-                (K, {_, V, _, F}) -> 
-                     spawn(fun() -> F(K, V, expire) end),
+                (K, {_, _, _, F}) ->
+                     spawn(fun() ->
+                                   {ok, Oa} = riakc_pb_socket:get(Pid, Bucket, K),
+                                   _ = riakc_pb_socket:delete(Pid, Bucket, K),
+                                   F(decode(K), decode(riakc_obj:get_value(Oa)), expire)
+                           end),
                      false
              end,
     _ = erlang:send_after(?EXPIRE_CHECK, self(), flush),
-    {noreply, dict:filter(Filter, Dict), hibernate};
+    {noreply, State#state{keys=dict:filter(Filter, Dict)}, hibernate};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -271,3 +337,7 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+encode(V) ->
+    term_to_binary(V, [compressed]).
+decode(V) ->
+    binary_to_term(V).
